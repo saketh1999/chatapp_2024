231a9f9db0b752a5e7532a9263e84e9c207c4939
SRI SAI SAKETH <srisaisakethcode@gmail.com>
2024-05-03 16:46:19 -0700

    ChatApp2024 Initial Commit

---
+++ app/src/main/java/com/example/chatapp_2024/LoginPhoneNumber.java
@@ -0,0 +1,16 @@
+package com.example.chatapp_2024;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginPhoneNumber extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_phone_number);
+
+    }
+
+}
